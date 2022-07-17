b29b0bf133f6c63e4f5b339469a61f35814380be
evgenij <mwbox89@gmail.com>
2022-07-18 00:22:40 +0300

    some fix

--- build/js/main.js
+++ build/js/main.js
@@ -12,9 +12,7 @@
 
   // import './helpers/postDate';
 
-  function main() {
-    scrollSmooth();
-  }
+  function main() {}
 
   if (document.documentElement.clientWidth < 480) {
     window.addEventListener('scroll', function () {
@@ -22,8 +20,6 @@
     }, {
       once: true
     });
-  } else {
-    main();
   }
   var wow = new WOW({
     boxClass: 'wow',
@@ -46,21 +42,16 @@
       slidesToScroll: 1,
       adaptiveHeight: true,
       arrows: true
-    });
-    $(".twentytwenty-container").twentytwenty({
-      default_offset_pct: 0.5,
-      // How much of the before image is visible when the page loads
-      orientation: 'horizontal',
-      // Orientation of the before and after images ('horizontal' or 'vertical')
-      no_overlay: true,
-      //Do not show the overlay with before and after
-      move_slider_on_hover: true,
-      // Move slider on mouse hover?
-      move_with_handle_only: true,
-      // Allow a user to swipe anywhere on the image to control slider movement. 
-      click_to_move: false // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
+    }); // $(".twentytwenty-container").twentytwenty({
+    //   default_offset_pct: 0.5, // How much of the before image is visible when the page loads
+    //   orientation: 'horizontal', // Orientation of the before and after images ('horizontal' or 'vertical')
+    //   no_overlay: true, //Do not show the overlay with before and after
+    //   move_slider_on_hover: true, // Move slider on mouse hover?
+    //   move_with_handle_only: true, // Allow a user to swipe anywhere on the image to control slider movement. 
+    //   click_to_move: false // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
+    // });
 
-    });
+    scrollSmooth();
   });
 
   function hideLoader() {
--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 import scrollSmooth from './helpers/smooth-scroll.js';
 
 function main() {
-  scrollSmooth();
+  
 }
 
 if (document.documentElement.clientWidth < 480) {
@@ -36,16 +36,18 @@ window.addEventListener("load", function() {
     adaptiveHeight: true,
     arrows: true
   });
-  $(".twentytwenty-container").twentytwenty({
-    default_offset_pct: 0.5, // How much of the before image is visible when the page loads
-    orientation: 'horizontal', // Orientation of the before and after images ('horizontal' or 'vertical')
-    no_overlay: true, //Do not show the overlay with before and after
-    move_slider_on_hover: true, // Move slider on mouse hover?
-    move_with_handle_only: true, // Allow a user to swipe anywhere on the image to control slider movement. 
-    click_to_move: false // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
-  });
 
 
+  // $(".twentytwenty-container").twentytwenty({
+  //   default_offset_pct: 0.5, // How much of the before image is visible when the page loads
+  //   orientation: 'horizontal', // Orientation of the before and after images ('horizontal' or 'vertical')
+  //   no_overlay: true, //Do not show the overlay with before and after
+  //   move_slider_on_hover: true, // Move slider on mouse hover?
+  //   move_with_handle_only: true, // Allow a user to swipe anywhere on the image to control slider movement. 
+  //   click_to_move: false // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
+  // });
+
+  scrollSmooth();
 })
 
 
